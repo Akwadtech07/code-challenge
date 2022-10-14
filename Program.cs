41207ef6a8ba1c41b619350f86b4933baa3d3481
unknown <akwadtech.surveillance@yahoo.com>
2022-10-14 18:34:51 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace code_challenge_sgas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subtraction();
+        }
+
+        public static void Subtraction()
+        {
+            Console.WriteLine("Enter firstNumber:");
+            int Num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter secondNumber:");
+            int Num2= int.Parse(Console.ReadLine());
+            if(Num1>=Num2)
+            {
+                Console.Write(Num2 -  Num1);
+            }
+            else 
+            {
+                Console.Write(Num1- Num2);
+                
+            }
+             
+             if(Num1 == Num2)
+             {
+                Console.Write(Num1- Num2);
+                
+             }
+
+           
+        
+        } 
+    //  question 2
+           public static void tittle()
+           {
+                string[] abc = new string[0];
+                 
+                
+            
+            
+                
+            
+
+            
+
+           }
+
+    }
+}
+ 
